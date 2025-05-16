d80b748537355ceb2972e3766b000c775fd4c054
Emelda-debug <teacayemmie@gmail.com>
2025-05-16 23:56:46 +0200

    coming soon and AI hardware page done

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def hardware():
     return render_template('hardware.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+@app.route('/coming_soon')
+def coming_soon():
+    return render_template('soon.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
