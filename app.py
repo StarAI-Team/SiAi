8482ce349aa4425abf3d816503d36207130a3b1b
emelda-debug <teacayemmie@gmail.com>
2025-05-16 17:56:35 +0200

    more chnages

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/hardware')
+def hardware():
+    return render_template('hardware.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
